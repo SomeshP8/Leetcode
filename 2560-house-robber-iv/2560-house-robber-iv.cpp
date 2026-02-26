455d0ab5aaeaff5c889f9578ce708a4959207bfa
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-02-26 14:56:05 +0530

    Time: 17 ms (81.07%), Space: 61 MB (12.93%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -1,28 +1,28 @@
 class Solution {
-public: 
-    bool rob(vector<int>& nums,int k,int can){
-        int c=0;
-        for(int i=0;i<nums.size();i++){
-        if(nums[i]<=can){
-          c++;
-          i++;
+public:
+    bool rob(vector<int>& nums, int k, int can) {
+        int c = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] <= can) {
+                c++;
+                i++;
+            }
+            if (c >= k)
+                return true;
         }
-        if(c>=k) return true;
-        }
-      return false;
+        return false;
     }
     int minCapability(vector<int>& nums, int k) {
-        int low=*min_element(nums.begin(),nums.end());
-        int high=*max_element(nums.begin(),nums.end());
-        int ans=high;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(rob(nums,k,mid)){
-                ans=mid;
-                high=mid-1;
-            }
-            else
-              low=mid+1;
+        int low = *min_element(nums.begin(), nums.end());
+        int high = *max_element(nums.begin(), nums.end());
+        int ans = high;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (rob(nums, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
         return low;
     }
