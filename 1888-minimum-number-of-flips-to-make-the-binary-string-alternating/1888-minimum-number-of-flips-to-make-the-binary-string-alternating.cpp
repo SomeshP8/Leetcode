8a097d6b6350c3a33c0c19f5429915e8191b8d44
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-03-07 09:59:33 +0530

    Time: 47 ms (25.2%), Space: 28.4 MB (20.8%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -5,7 +5,7 @@ public:
         s=s+s;
         string s1="";
         string s2="";
-        for(int i=0;i<n;i++){
+        for(int i=0;i<2*n;i++){
             if(i%2==0){
                 s1+="0";
                 s2+="1";
@@ -18,12 +18,13 @@ public:
         int d1=0,d2=0;
         int left=0;
         int ans=INT_MAX;
-        for(int right=0;right<n;right++){
+        for(int right=0;right<2*n;right++){
             if(s[right]!=s1[right]) d1++;
             if(s[right]!=s2[right]) d2++;
             if(right-left+1>n){
                 if(s[left]!=s1[left]) d1--;
                 if(s[left]!=s2[left]) d2--;
+                left++;
             }
             if(right-left+1==n){
                 ans=min(ans,min(d1,d2));
