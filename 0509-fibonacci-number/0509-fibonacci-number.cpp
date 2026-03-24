0cec95b7826fcb0243e87bd256908a6e6117cb01
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-03-24 18:33:52 +0530

    Time: 3 ms (65.33%), Space: 8.2 MB (15.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
+        if(n==0) return 0;
+        if(n==1) return 1;
         vector<int>dp(n+1);
         dp[0]=0;
         dp[1]=1;
