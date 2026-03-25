121d0c6d9de2801d4c26fb4cac76fbfafdb3b983
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-03-25 10:20:02 +0530

    Time: 31 ms (95.72%), Space: 14.7 MB (77.22%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int x:nums) sum+=x;
+        if(sum%2!=0) return false;
+        int target=sum/2;
+        vector<int>dp(target+1,0);
+        dp[0]=true;
+        for(int num:nums){
+            for(int i=target;i>=num;i--){
+                dp[i]=dp[i] || dp[i-num];
+            }
+        }
+        return dp[target];
+    }
+};
