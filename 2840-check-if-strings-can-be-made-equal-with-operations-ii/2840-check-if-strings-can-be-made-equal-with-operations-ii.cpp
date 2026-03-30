fa340b5cdd65c9fe1b456aa6a04ab504346c9ba4
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-03-30 18:38:13 +0530

    Time: 68 ms (10.35%), Space: 24.8 MB (25.29%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        int freq[52] = {};
+        array<string, 2> A, B;
         for (int i = 0; i < s1.length(); i++) {
-            int off = (i & 1) * 26;
-            freq[s1[i] - 'a' + off]++;
-            freq[s2[i] - 'a' + off]--;
+            int off = i & 1;
+            A[off] += s1[i];
+            B[off] += s2[i];
         }
-        for (int i = 0; i < 52; i++)
-            if (freq[i] != 0) return false;
-        return true;
+
+        for (int i = 0; i < 2; i++) {
+            sort(A[i].begin(), A[i].end());
+            sort(B[i].begin(), B[i].end());
+        }
+
+        return A == B;
     }
 };
