15576bd8aadd1beb12bc225d888267b2cf960a9e
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-09 21:17:16 +0530

    Time: 53 ms (30.88%), Space: 10.2 MB (29.64%) - LeetHub - Time: O, Space: O(?)

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int totalWaviness(int num1, int num2) {
-        int c=0;
-        for(int i=num1;i<=num2;i++){
-            string s=to_string(i);
-            int n=s.length();
-            if(n<3) continue;
-            for(int j=1;j<n-1;j++){
-                if(s[j]>s[j-1] && s[j]>s[j+1]) c++;
-                else if(s[j]<s[j-1] && s[j]<s[j+1]) c++;
+    int waviness(int x) {
+        string s = to_string(x);
+        int cnt = 0;
+        for(int i = 1; i < (int)s.size() - 1; i++) {
+            if ((s[i] > s[i - 1] && s[i] > s[i + 1]) ||
+                (s[i] < s[i - 1] && s[i] < s[i + 1])) {
+                cnt++;
             }
         }
-        return c;
+        return cnt;
+    }
+    int totalWaviness(int num1, int num2) {
+        long long ans = 0;
+        for (int x = num1; x <= num2; x++) {
+            ans += waviness(x);
+        }
+        return ans;
     }
 };
 
