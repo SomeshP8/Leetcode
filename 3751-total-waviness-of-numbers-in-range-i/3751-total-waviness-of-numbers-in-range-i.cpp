8d2aa99bab4471a626893da37cd0e752c3fea70a
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-09 21:20:32 +0530

    Time: 9 ms (82.2%), Space: 9.6 MB (51.53%) - LeetHub - Time: O(, Space: O(?)

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,22 +1,67 @@
 class Solution {
 public:
-    int waviness(int x) {
-        string s = to_string(x);
-        int cnt = 0;
-        for(int i = 1; i < (int)s.size() - 1; i++) {
-            if ((s[i] > s[i - 1] && s[i] > s[i + 1]) ||
-                (s[i] < s[i - 1] && s[i] < s[i + 1])) {
-                cnt++;
+    string s;
+    pair<long long, long long> dp[12][11][11][2][2];
+    bool vis[12][11][11][2][2];
+
+    pair<long long, long long> dfs(int pos, int prev1, int prev2, bool tight, bool started) {
+        if (pos == s.size()) {
+            return {1, 0}; 
+        }
+
+        if (vis[pos][prev1][prev2][tight][started])
+            return dp[pos][prev1][prev2][tight][started];
+
+        long long totalCount = 0, totalWave = 0;
+
+        int limit = tight ? s[pos] - '0' : 9;
+
+        for (int d = 0; d <= limit; d++) {
+            bool newTight = tight && (d == limit);
+            bool newStarted = started || (d != 0);
+
+            int nprev1 = prev1, nprev2 = prev2;
+
+            long long add = 0;
+
+            if (!newStarted) {
+                nprev1 = nprev2 = 10; 
+            } else {
+                if (started) {
+                    nprev2 = prev1;
+                    nprev1 = d;
+
+                    if (prev2 != 10) {
+                        if ((prev1 > prev2 && prev1 > d) ||
+                            (prev1 < prev2 && prev1 < d)) {
+                            add = 1;
+                        }
+                    }
+                } else {
+                    nprev1 = d;
+                    nprev2 = 10;
+                }
             }
+
+            auto [cnt, wave] = dfs(pos + 1, nprev1, nprev2, newTight, newStarted);
+
+            totalCount += cnt;
+            totalWave += wave + cnt * add;
         }
-        return cnt;
+
+        vis[pos][prev1][prev2][tight][started] = true;
+        return dp[pos][prev1][prev2][tight][started] = {totalCount, totalWave};
     }
+
+    long long solve(long long x) {
+        if (x < 0) return 0;
+        s = to_string(x);
+        memset(vis, false, sizeof(vis));
+        return dfs(0, 10, 10, 1, 0).second;
+    }
+
     int totalWaviness(int num1, int num2) {
-        long long ans = 0;
-        for (int x = num1; x <= num2; x++) {
-            ans += waviness(x);
-        }
-        return ans;
+        return solve(num2) - solve(num1 - 1);
     }
 };
 
