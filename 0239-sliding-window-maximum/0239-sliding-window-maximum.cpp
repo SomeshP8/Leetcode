a80aebce5399d2dd5fcf71ba6c5dcce0abffd94f
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-14 21:13:01 +0530

    Time: 12 ms (95.3%), Space: 139.3 MB (24.81%) - LeetHub - Time: O(n), Space: O(k)

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+         deque<int> dq;
+
+        // Result vector to store the maximums
+        vector<int> result;
+
+        // Loop through each element in the array
+        for (int i = 0; i < nums.size(); i++) {
+            // Remove elements from the front if they are out of this window's range
+            if (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // Remove all elements from the back that are smaller than current element
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            // Add the current index to the deque
+            dq.push_back(i);
+
+            // Once the first window is completed, add front element to result
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+
+        // Return the final result
+        return result;  
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
