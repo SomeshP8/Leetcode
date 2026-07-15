181309dd706359fc93273c0e457bc7cd6fdb1b09
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-15 22:47:28 +0530

    Time: 251 ms (54.01%), Space: 10.6 MB (65.52%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+   vector<vector<int>>direction{{1,0},{-1,0},{0,1},{0,-1}};
+   bool find(vector<vector<char>>& board,int i,int j,int idx,string& word,int n,int m)
+    {
+    if(idx==word.size()) return true;
+    if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='$') return false;
+    if(board[i][j]!=word[idx]) return false;
+    char temp=board[i][j];
+    board[i][j]='$';
+    for(auto &dir:direction){
+        int new_i=i+dir[0];
+        int new_j=j+dir[1];
+        if(find(board,new_i,new_j,idx+1,word,n,m)) return true;
+    }
+    board[i][j]=temp;
+    return false;
+   }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size(),m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0] && find(board,i,j,0,word,n,m)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
