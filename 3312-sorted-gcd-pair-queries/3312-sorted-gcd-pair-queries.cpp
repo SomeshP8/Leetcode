ab19edb803aa08802722cf0eab3d4cb462d7cc5f
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-17 09:32:06 +0530

    Time: 45 ms (60%), Space: 123.1 MB (82.67%) - LeetHub - Time: O(m + n log m + q log m), Space: O(m)

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+    int m = *max_element(nums.begin(), nums.end());
+    vector<long long>cnt(m+1,0);
+    for(int num:nums) cnt[num]++;
+    for(int i=1;i<=m;i++){
+        for(int j=2*i;j<=m;j+=i) cnt[i]+=cnt[j];
+    }
+    for(int i=1;i<=m;i++) cnt[i]=cnt[i]*(cnt[i]-1)/2;
+    for(int i=m;i>=1;i--){
+        for(int j=2*i;j<=m;j+=i) cnt[i]-=cnt[j];
+    }
+    for(int i=1;i<=m;i++) cnt[i]+=cnt[i-1];
+    vector<int>ans;
+    for(long long q:queries){
+        long long t=q+1;
+        int pos=lower_bound(cnt.begin(),cnt.end(),t)-cnt.begin();
+        ans.push_back(pos);
+    }
+    return ans;
+  }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
