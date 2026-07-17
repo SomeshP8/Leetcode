c74dc6c05f40fe77960cddbf672f61ef79a353c5
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-17 13:12:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (6.63%) - LeetHub - Time: O(log n), Space: O(1)

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,3 +4,7 @@ public:
        return pow(x,n);
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
