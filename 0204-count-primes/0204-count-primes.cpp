3c5602878de57fa1432847087cf485a2a1047a36
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-17 15:02:47 +0530

    Time: 171 ms (57.77%), Space: 12.5 MB (91.75%) - LeetHub - Time: O(n log log n), Space: O(n)

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int countPrimes(int n) {
-     int c=0;
-     vector<bool>prime(n+1,true);
-     prime[0]=prime[1]=false;
-     for(int i=2;i<n;i++){
-        if(prime[i]){
-            c++;
-            for(int j=i*2;j<n;j+=i){
-              prime[j]=0;  
+        if (n <= 2) return 0;
+
+        vector<bool> isPrime(n, true);  // internally bit-optimized
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-     }
-     return c;
+
+        int count = 0;
+        for (bool x : isPrime) {
+            if (x) count++;
+        }
+
+        return count;
     }
 };
 
