d006143a4bef35655aa7d890a946eeb561d3b3ce
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-07-19 08:15:44 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.83%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+         vector<int>last(26,0);
+         vector<bool>seen(26,false);
+         string result="";
+         for(int i=0;i<s.length();i++){
+            last[s[i]-'a']=i;
+         }
+         for(int i=0;i<s.length();i++){
+            char c=s[i];
+            if(seen[c-'a']) continue;
+            while(!result.empty() && result.back()>c && last[result.back()-'a']>i){
+                seen[result.back()-'a']=false;
+                result.pop_back();
+            }
+            result.push_back(c);
+            seen[c-'a']=true;
+         }
+         return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
