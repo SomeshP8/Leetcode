adf22bfc43a7dceea2d8326a7a7deb837b102c43
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-08-19 08:31:12 +0530

    Time: 114 ms (13.34%), Space: 83.4 MB (24.16%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,36 @@
+
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reserved) {
+        int ans = 0;
+        unordered_map<int,vector<int>> mp;
+        for(auto r: reserved){
+            mp[r[0]].push_back(r[1]);
+        }
+        ans = (n-mp.size())*2 ; 
+        for(auto r : mp){
+            bool flag = 0;
+            vector<bool> contains(11 , 0);
+            for(int x : r.second) {
+                contains[x] = 1;
+            }
+            if(!contains[2] && !contains[3] && !contains[4] && !contains[5])  {
+                ans++;
+                flag = 1;
+            }
+            if(!contains[6] && !contains[7] && !contains[8] && !contains[9]) {
+                ans++;
+                flag = 1;
+            }
+            if(!flag) {
+                if(!contains[4] && !contains[5] && !contains[6] && !contains[7])   
+                    ans++;
+            }            
+        }
+        return ans; 
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
