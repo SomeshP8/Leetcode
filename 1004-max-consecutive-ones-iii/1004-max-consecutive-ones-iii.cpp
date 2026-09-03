4c0001749d227f0ba54cd8b49b4fa1fba409b2ff
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-09-03 18:24:58 +0530

    Time: 0 ms (100%), Space: 69.5 MB (70.02%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l = 0, r = 0, maxi = 0, zero = 0;
+        while (r < nums.size()) {
+            if (nums[r] == 0) {
+                zero++;
+            }
+            while (zero > k) {
+                if (nums[l] == 0) {
+                    zero--;
+                }
+                l++;
+            }
+            maxi = max(maxi, r - l + 1);
+            r++;
+        }
+        return maxi;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
