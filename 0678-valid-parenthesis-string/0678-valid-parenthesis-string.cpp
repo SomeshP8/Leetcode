7a65ab8b079676388592ddde6a7d94daa42fe34d
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-09-06 14:40:23 +0530

    Time: 0 ms (100%), Space: 8 MB (74.7%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
