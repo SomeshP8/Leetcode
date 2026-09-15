154423e1e971412257c0a0f5222a47706fd9d199
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-09-15 10:16:41 +0530

    Time: 0 ms (100%), Space: 21.6 MB (76.23%) - LeetHub - Time: O(n), Space: O(n)

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
-
-        vector<vector<int>> res;
-        res.push_back(intervals[0]);
-
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (res.back()[1] >= intervals[i][0]) {
-                res.back()[1] = max(res.back()[1], intervals[i][1]);
-            } else {
-                res.push_back(intervals[i]);
-            }
-        }
-
-        return res;        
+    vector<vector<int>>res;
+    int n=intervals.size(),i=0;
+    while(i<n && intervals[i][1]<newInterval[0]){
+        res.push_back(intervals[i]);
+        i++;
     }
-};
+    while(i<n && intervals[i][0]<=newInterval[1]){
+        newInterval[0]=min(newInterval[0],intervals[i][0]);
+        newInterval[1]=max(newInterval[1],intervals[i][1]);
+        i++;
+    }
+    res.push_back(newInterval);
+    while(i<n){
+        res.push_back(intervals[i]);
+        i++;
+    }
+    return res;
+    }
+ };
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
