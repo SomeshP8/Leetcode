1caca563d996798cd6b92cdbe304aefe3568b398
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-09-16 18:04:33 +0530

    Time: 0 ms (100%), Space: 73.1 MB (96.95%) - LeetHub - Time: O(n^2), Space: O(n^2)

---
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> cyclicShift(int n, vector<vector<int>>& grid, vector<int>& rowShift, vector<int>& colShift) {
+       vector<vector<int>> result(n, vector<int>(n));
+        for (int r = 0; r < n; ++r)
+            for (int c = 0; c < n; ++c) {
+                int nc = (c - rowShift[r] % n + n) % n;
+                int nr = (r - colShift[nc] % n + n) % n;
+                result[nr][nc] = grid[r][c];
+            }
+        return result; 
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
