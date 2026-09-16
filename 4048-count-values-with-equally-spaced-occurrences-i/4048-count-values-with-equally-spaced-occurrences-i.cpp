16a34a09dffa83ba3a5e2d7322650a1c8283e857
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-09-16 18:49:17 +0530

    Time: 3 ms (95.16%), Space: 32.2 MB (66.56%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+     
+    int countSpecialIntegers(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> mp(101);
+        for(int i=0;i<n;i++) mp[nums[i]].push_back(i);
+        int cnt=0;
+        for(int i=0;i<=100;i++){
+            if(mp[i].size()==3){
+                if(mp[i][1]-mp[i][0]==mp[i][2]-mp[i][1]) cnt++;
+            }
+        }
+        cout<<cnt;
+        return cnt;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
