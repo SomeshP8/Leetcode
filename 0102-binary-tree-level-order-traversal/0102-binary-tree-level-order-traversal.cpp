e454862375e3516ab5277dc4642af1dc8ce85de4
Yagati Someswara Rao <someswararaoyagati@gmail.com>
2026-09-17 18:13:50 +0530

    Time: 6 ms (5.9%), Space: 17.2 MB (15.79%) - LeetHub - Time: O(n), Space: O(n)

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,22 +12,26 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==nullptr) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>level;
-            for(int i=0;i<size;i++){
-                TreeNode *node=q.front();
-                q.pop();
-                if(node->left!=nullptr) q.push(node->left);
-                if(node->right!=nullptr) q.push(node->right);
-                level.push_back(node->val); 
-            }
-            ans.push_back(level);
+       vector<vector<int>>ans;
+       if(root==nullptr) return ans;
+       queue<TreeNode*>q;
+       q.push(root);
+       while(!q.empty()){
+        int size=q.size();
+        vector<int>level;
+        for(int i=0;i<size;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left!=nullptr) q.push(node->left);
+            if(node->right!=nullptr) q.push(node->right);
+            level.push_back(node->val);
         }
-        return ans;
+        ans.push_back(level);
+       }
+       return ans;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
